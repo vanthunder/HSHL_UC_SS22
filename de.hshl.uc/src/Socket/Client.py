0fa66058e3791c6180626263bd4ccec5fc9c258e
vanthunder <47546768+vanthunder@users.noreply.github.com>
2022-04-21 00:21:54 +0200

    Server send and receive!

--- de.hshl.uc/src/Socket/Client.py
+++ de.hshl.uc/src/Socket/Client.py
@@ -6,6 +6,12 @@ server_adr = ('34.159.99.140', 1666)
 client_socket.connect(server_adr)
 print(str(socket.gethostname()))
 client_socket.send(bytes('Hi Marvin', "utf8"))
+msg = 'Test'
+while True:
+    print(client_socket.recv(2048))
+    #msg = input('Eingabe: ')
+    #print(msg, 'Client')
+    client_socket.send(bytes('Test', "utf8"))
+    print("Send To Server!")
 
-print(client_socket.recv(2048))
-
+print("False")
