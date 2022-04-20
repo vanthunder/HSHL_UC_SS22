ab6cdf07406ed35e9987b80c029f381bc5ffcb55
vanthunder <47546768+vanthunder@users.noreply.github.com>
2022-04-20 22:43:25 +0200

    Server send and receive!

--- de.hshl.uc/src/Socket/Client.py
+++ de.hshl.uc/src/Socket/Client.py
@@ -5,5 +5,7 @@ client_socket = socket.socket(socket.AF_INET,
 server_adr = ('34.159.99.140', 1666)
 client_socket.connect(server_adr)
 print(str(socket.gethostname()))
-client_socket.send(bytes('Hi', "utf8"))
+client_socket.send(bytes('Hi Marvin', "utf8"))
+
+print(client_socket.recv(2048))
 
