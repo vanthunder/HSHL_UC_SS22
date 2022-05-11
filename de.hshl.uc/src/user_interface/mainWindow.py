1fd56e6fd90d08a9092275c11e8b2b41a2e5bd81
vanthunder <47546768+vanthunder@users.noreply.github.com>
2022-05-12 00:12:54 +0200

    Updates the UI!

--- de.hshl.uc/src/user_interface/mainWindow.py
+++ de.hshl.uc/src/user_interface/mainWindow.py
@@ -115,6 +115,8 @@ class StartWindow(QMainWindow):
         super().__init__()
         self.window_title = 'start'
         self.fontA = QFont("Josefin Sans Medium", 24)
+        self.fontB = QFont("Josefin Sans Medium", 100)
+        self.fontC = QFont("Josefin Sans Medium", 40)
         self.bX = 0
         self.bY = 0
         self.positive = True
@@ -150,11 +152,16 @@ class StartWindow(QMainWindow):
 
         # Info Label
         self.info_Label_Container = QLabel()
-        self.info_Label_Container.setStyleSheet("margin-left: 10px; border-radius: 25px; background: #8BC1E9; color: black;")
+        self.info_Label_Container.setStyleSheet("margin: 20px 40px; border-radius: 25px; background: #8BC1E9; color: black;")
+        #self.info_Label_Container.setMaximumHeight(400)
         self.info_Label_Container.setFont(self.fontA)
         #self.info_Label_Container.setMaximumSize(100, 400)
         self.info_Label_Container.setAutoFillBackground(True)
         #self.info_Label_Container.setStyleSheet("""background: #ebef00;""")
+        # date and temp vBox
+        self.clock_temp_vbox = QLabel()
+        self.clock_temp_vbox.layout = QVBoxLayout(self.clock_temp_vbox)
+
         # Date Label
         self.date_label = QLabel()
         self.date_label.setText("Montag")
@@ -162,15 +169,26 @@ class StartWindow(QMainWindow):
         # Clock
         self.clock_label = QLabel()
         self.clock_label.setText("20:00")
-        self.clock_label.setFont(self.fontA)
+        self.clock_label.setAlignment(QtCore.Qt.AlignCenter)
+        #self.clock_label.setMinimumWidth(400)
+        self.clock_label.setFont(self.fontB)
+        self.clock_label.setStyleSheet("margin-bottom: 0px; color: white")
         # Temp
         self.temp_label = QLabel()
         self.temp_label.setText("24°C")
-        self.temp_label.setFont(self.fontA)
+        self.temp_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.temp_label.setFont(self.fontC)
+        self.temp_label.setStyleSheet("margin-top: 0px; color: white")
         # Fact Label
         self.fact_label = QLabel()
-        self.fact_label.setText("Lorem ipsum dolor sit amet, \nconsetetur sadipscing elitr, \nsed diam nonumy eirmod tempor invidunt \nut labore et dolore magna aliquyam \nerat, sed diam voluptua. \nAt vero eos et accusam et justo duo dolores et ea rebum. \nStet clita kasd gubergren, \nno sea takimata sanctus est Lorem ipsum dolor sit amet.")
+        self.fact_label.setText("Lorem ipsum dolor sit amet, \nconsetetur sadipscing elitr, \nsed diam nonumy eirmod tempor invidunt \nut labore et dolore magna aliquyam \nerat, sed diam voluptua.")
         self.fact_label.setFont(self.fontA)
+        # Adds Clock and Temp to the vbox
+        self.clock_temp_vbox.layout.addWidget(self.clock_label)
+        self.clock_temp_vbox.layout.addWidget(self.clock_label)
+        self.clock_temp_vbox.layout.addWidget(self.temp_label)
+        self.clock_temp_vbox.setMinimumWidth(800)
+        #self.clock_temp_vbox.setStyleSheet("overflow: hidden;border-radius: 25px; background: #F7AF9D; color: black;")
 
         # Hbox
         self.mid_label_container = QLabel()
@@ -178,13 +196,18 @@ class StartWindow(QMainWindow):
         # inner vbox
         self.inner_vbox_label_container = QLabel()
         self.inner_vbox_label_container.layout = QVBoxLayout(self.inner_vbox_label_container)
+        self.inner_vbox_label_container.setAlignment(QtCore.Qt.AlignCenter)
         # Adds Buttons to the inner box
         self.button_Opinion = QPushButton('Meinungsumfrage', self.inner_vbox_label_container)
-        self.button_Opinion.setStyleSheet("background-color: #B28BBC; border-style: thin; border-color: black; border-width: 5px; border-radius: 10px;")
-        self.button_Opinion.setMinimumSize(200,200)
+        self.button_Opinion.setStyleSheet("margin-left: 20px 40px; background-color: #B28BBC; border-style: thin; border-color: black; border-width: 5px; border-radius: 24px;")
+        self.button_Opinion.setMinimumSize(100, 250)
+        self.button_Opinion.setMaximumSize(400, 250)
+        self.button_Opinion.setFont(self.fontA)
         self.button_Play = QPushButton('Spielesammlung', self.inner_vbox_label_container)
-        self.button_Play.setStyleSheet("background-color: #4B6E74; border-style: thin; border-color: black; border-width: 5px; border-radius: 10px;")
-        self.button_Play.setMinimumSize(200, 200)
+        self.button_Play.setStyleSheet("margin-left: 20px 40px; background-color: #4B6E74; border-style: thin; border-color: black; border-width: 5px; border-radius: 24px;")
+        self.button_Play.setMinimumSize(100, 250)
+        self.button_Play.setMaximumSize(400, 250)
+        self.button_Play.setFont(self.fontA)
         self.inner_vbox_label_container.layout.addWidget(self.button_Opinion)
         self.inner_vbox_label_container.layout.addWidget(self.button_Play)
         # Adds the inner box to the outer box
@@ -193,11 +216,11 @@ class StartWindow(QMainWindow):
         # outer box
         self.outer_chat_v_label = QLabel()
         self.outer_chat_v_label.layout = QVBoxLayout(self.outer_chat_v_label)
-        self.outer_chat_v_label.setStyleSheet("border-radius: 25px; background: #F7AF9D; color: black;")
+        self.outer_chat_v_label.setStyleSheet("overflow: hidden;border-radius: 25px; background: #F7AF9D; color: black;")
         # inner box
         self.inner_chat_label = QLabel()
         self.inner_chat_label.setText("Lorem ipsum dolor sit amet, \nconsetetur sadipscing elitr, \nsed diam nonumy eirmod tempor invidunt \nut labore et dolore magna aliquyam \nerat, sed diam voluptua.")
-        self.inner_chat_label.setStyleSheet("border-radius: 25px; background: #EBEFF0; color: black;")
+        self.inner_chat_label.setStyleSheet("float: left; left: 20px; padding-right: opx; border-radius: 25px; background: #EBEFF0; color: black;")
         self.inner_chat_label.setMinimumWidth(800)
         self.inner_chat_label.setFont(self.fontA)
         # Adds the inner box to the outer box
@@ -280,8 +303,7 @@ class StartWindow(QMainWindow):
         self.wid_start.layout.addWidget(self.info_Label_Container)
         self.info_Label_Container.layout = QHBoxLayout(self.info_Label_Container)
         self.info_Label_Container.layout.addWidget(self.date_label)
-        self.info_Label_Container.layout.addWidget(self.clock_label)
-        self.info_Label_Container.layout.addWidget(self.temp_label)
+        self.info_Label_Container.layout.addWidget(self.clock_temp_vbox)
         self.info_Label_Container.layout.addWidget(self.fact_label)
         self.wid_start.layout.addWidget(self.mid_label_container)
         self.wid1.layout = QVBoxLayout(self.wid1)
