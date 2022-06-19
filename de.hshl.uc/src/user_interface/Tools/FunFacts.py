71e233d8629ac612c16c7efcdd20bdbb8298e323
vanthunder <47546768+vanthunder@users.noreply.github.com>
2022-06-19 22:02:28 +0200

    Updates Goal Counter!

---
+++ de.hshl.uc/src/user_interface/Tools/FunFacts.py
@@ -0,0 +1,10 @@
+from PyQt5.QtGui import QMovie
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QStackedLayout
+from PyQt5.QtWidgets import QLabel, QVBoxLayout
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Cursor():
+    def __init__(self):
+        super().__init__()
+        self.funFacts = [1,1,1,1]
