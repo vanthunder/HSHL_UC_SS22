8bc737b2fb9b2b820da70ef5a95ccd3f502fa7e8
vanthunder <47546768+vanthunder@users.noreply.github.com>
2022-06-27 20:40:25 +0200

    Implements new goal method!

--- de.hshl.uc/src/user_interface/mainWindow.py
+++ de.hshl.uc/src/user_interface/mainWindow.py
@@ -461,7 +461,7 @@ class StartWindow(QMainWindow):
                 #self.String("torL")
                 self.local_cL.sendCollision("torL")
                 #self.scoreRightCounter += 1
-                self.pongWindow.scoreRight.setText(str(20))
+                #self.pongWindow.scoreRight.setText(str(20))
                 self.scoreRight = True
                 self.updateTor
                 #time.sleep(0.5)
@@ -481,21 +481,33 @@ class StartWindow(QMainWindow):
 
 
     def updateTor(self):
+        if self.scoreLeft:
+            self.scoreLeft = False
+            self.scoreRight = False
+            self.scoreLeftCounter += 1
+            self.pongWindow.scoreLeft.setText(str(self.scoreLeftCounter))
+            self.goalGlobalCounter = 0
+        if self.scoreRight:
+            self.scoreLeft = False
+            self.scoreRight = False
+            self.scoreRightCounter += 1
+            self.pongWindow.scoreRight.setText(str(self.scoreRightCounter))
+            self.goalGlobalCounter = 0
         if self.goalGlobalCounter <= 20:
             self.goalGlobalCounter += 1
         if self.goalGlobalCounter == 20:
             #TODO: Put Goal Code Here:
             if self.scoreLeft:
-                self.scoreLeft = False
-                self.scoreRight = False
-                self.scoreLeftCounter +=1
-                self.pongWindow.scoreLeft.setText(str(self.scoreLeftCounter))
+                #self.scoreLeft = False
+                #self.scoreRight = False
+                #self.scoreLeftCounter +=1
+                #self.pongWindow.scoreLeft.setText(str(self.scoreLeftCounter))
                 self.goalGlobalCounter = 0
             if self.scoreRight:
-                self.scoreLeft = False
-                self.scoreRight = False
-                self.scoreRightCounter += 1
-                self.pongWindow.scoreRight.setText(str(self.scoreRightCounter))
+                #self.scoreLeft = False
+                #self.scoreRight = False
+                #self.scoreRightCounter += 1
+                #self.pongWindow.scoreRight.setText(str(self.scoreRightCounter))
                 self.goalGlobalCounter = 0
             #self.scoreLeft = False
             #self.scoreRight = False
