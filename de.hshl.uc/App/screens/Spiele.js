7c6fd545f98f5fbcbec8b62acf1dee55b25c23b4
RavenNight94 <104899981+RavenNight94@users.noreply.github.com>
2022-07-05 23:29:12 +0200

    Update Spiele.js
    Fertiges Design

--- de.hshl.uc/App/screens/Spiele.js
+++ de.hshl.uc/App/screens/Spiele.js
@@ -1,42 +1,72 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { globalStyles } from "../styles/gobal";
+import { StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
-export default function Spiele(navigation) {
-  const pressUmfrage = () => {
-    navigation.navigate('Umfrage')
-  }
-  const pressSpiele = () => {
-    navigation.navigate('Spiele')
-  }
-  const pressChat = () => {
-    navigation.navigate('Chat')
-  }
+
+export default function Spiele() {
   return (
-<View style={globalStyles.containerTwo}>
-       <Text style={globalStyles.boxFour}>Chat</Text>
-       <View style={globalStyles.Spieleauswahl}>
-  <TouchableOpacity
-   style={globalStyles.boxTwo}
-   onPress={(pressUmfrage)}>
-   <Text  style={globalStyles.Titel}>Phong</Text>
-   </TouchableOpacity>
-
     
- 
-   <TouchableOpacity
-   style={globalStyles.boxThree}
-   onPress={(pressSpiele)}>
-   <Text style={globalStyles.Titel}>TickTackToe</Text>
-   </TouchableOpacity>
-   
-   <TouchableOpacity
-   style={globalStyles.boxFive}
-   onPress={(pressChat)}>
-   <Text style={globalStyles.Titel}>x</Text>
-   </TouchableOpacity>
-   </View>
-  </View>
+     <View style={styles.container}>
+      <Text style={styles.boxOne}>Welches Spiel möchtest du spielen?</Text>
+     
+      <TouchableOpacity style={styles.boxTwo}> 
+      <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Phong</Text>
+      </TouchableOpacity>
+     
+      <TouchableOpacity style={styles.boxThree}> 
+      <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Tick Tack Toe</Text>
+      </TouchableOpacity>
+     
+      <TouchableOpacity style={styles.boxFour}>
+      <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}> C</Text>
+      </TouchableOpacity>
+    </View>
+
   );
 
-}
+}const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: '#EBEFF0',
+      paddingBottom: 30,
+      paddingTop: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
+    },
+    boxOne: {
+      flex:2,
+      backgroundColor: '#8BC1E9',
+      justifyContent: 'space-between',
+      textAlign: 'center',
+      textAlignVertical: 'center',
+      borderRadius: 25,
+      fontFamily: "JosefineSansMedium",
+      fontSize: 50,
+      marginBottom: 30,
+      paddingTop:10
+    },
+    boxTwo: {
+      flex:1,
+      backgroundColor: '#B28BBC',
+      borderTopLeftRadius: 25,
+      borderTopRightRadius: 25,
+      justifyContent:"center",
+      alignItems: "center",
+    
+    },
+    boxThree: {
+      flex:1,
+      backgroundColor: '#4B6E74',
+      justifyContent:"center",
+      alignItems: "center",
+    },
+    boxFour: {
+      flex:1,
+      backgroundColor: '#F7AF9D',
+      borderBottomLeftRadius: 25,
+      borderBottomRightRadius: 25,
+      justifyContent:"center",
+      alignItems: "center",
+    
+    },
+  });
+  
