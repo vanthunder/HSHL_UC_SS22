f3332c3c153ab76a668939156f9cb800d0248049
RavenNight94 <104899981+RavenNight94@users.noreply.github.com>
2022-07-05 23:13:47 +0200

    Update Umfrage.js
    Update für Styles

--- de.hshl.uc/App/screens/Umfrage.js
+++ de.hshl.uc/App/screens/Umfrage.js
@@ -1,28 +1,49 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
 
 
 export default function Umfrage() {
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text style={styles.boxOne}>Was ist deine Meingung?</Text>
-      <Text style={styles.boxTwo}>A</Text>
-      <Text style={styles.boxThree}>B</Text>
-      <Text style={styles.boxFour}>C</Text>
+    <Text style={styles.boxOne}>An welchem Campus studierst du?</Text>
+     
+     <TouchableOpacity style={styles.boxTwo}> 
+     <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Hamm</Text>
+     </TouchableOpacity>
+    
+     <TouchableOpacity style={styles.boxFive}> 
+     <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Lippstadt</Text>
+     </TouchableOpacity>
+     </View>
+     <View style={styles.container}>
+      <Text style={styles.boxOne}>Wo wohnst du?</Text>
+     
+      <TouchableOpacity style={styles.boxTwo}> 
+      <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Eigene Wohnung</Text>
+      </TouchableOpacity>
+     
+      <TouchableOpacity style={styles.boxThree}> 
+      <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Bei den Eltern</Text>
+      </TouchableOpacity>
+     
+      <TouchableOpacity style={styles.boxFour}>
+      <Text style={{fontFamily: 'JosefineSansMedium', fontSize:50}}>Wohnheim</Text>
+      </TouchableOpacity>
     </View>
+    </ScrollView>
   );
 
 }const styles = StyleSheet.create({
     container: {
-      flex: 1,
       backgroundColor: '#EBEFF0',
       paddingBottom: 30,
-      paddingTop: 30,
+      paddingTop: 20,
       paddingLeft: 20,
       paddingRight: 20,
     },
     boxOne: {
-      flex: 1,
+      height: 175,
       backgroundColor: '#8BC1E9',
       justifyContent: 'space-between',
       textAlign: 'center',
@@ -31,35 +52,40 @@ export default function Umfrage() {
       fontFamily: "JosefineSansMedium",
       fontSize: 50,
       marginBottom: 30,
+      paddingTop:10
     },
     boxTwo: {
-      flex: 1,
+      height: 125,
       backgroundColor: '#B28BBC',
-      textAlign: 'center',
-      textAlignVertical: 'center',
       borderTopLeftRadius: 25,
       borderTopRightRadius: 25,
-      fontFamily: "JosefineSansMedium",
-      fontSize: 50,
+      justifyContent:"center",
+      alignItems: "center",
+    
     },
     boxThree: {
-      flex: 1,
+      height: 125,
       backgroundColor: '#4B6E74',
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      fontFamily: "JosefineSansMedium",
-      fontSize: 50,
+      justifyContent:"center",
+      alignItems: "center",
     },
     boxFour: {
-      flex: 1,
+     height: 125,
       backgroundColor: '#F7AF9D',
       borderBottomLeftRadius: 25,
       borderBottomRightRadius: 25,
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      fontFamily: "JosefineSansMedium",
-      fontSize: 50,
-    }
+      justifyContent:"center",
+      alignItems: "center",
+    
+    },
+    boxFive: {
+      height: 125,
+      backgroundColor: '#4B6E74',
+      justifyContent:"center",
+      alignItems: "center",
+      borderBottomLeftRadius: 25,
+      borderBottomRightRadius: 25,
+    },
     
   });
   
